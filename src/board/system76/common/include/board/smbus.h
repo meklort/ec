4c1f363db28d22328d1aee418841ae3e7830248a
Evan Lojewski <github@meklort.com>
2020-05-02 09:49:30 -0600

    smbus: Allow arbitrary sizes to be read.

--- src/board/system76/common/include/board/smbus.h
+++ src/board/system76/common/include/board/smbus.h
@@ -6,5 +6,7 @@
 void smbus_init(void);
 int smbus_read(uint8_t address, uint8_t command, uint16_t * data);
 int smbus_write(uint8_t address, uint8_t command, uint16_t data);
+int smbus_read_bytes(uint8_t address, uint8_t command, char data[], uint8_t bytes);
+int smbus_write_bytes(uint8_t address, uint8_t command, char data[], uint8_t bytes);
 
 #endif // _BOARD_SMBUS_H
