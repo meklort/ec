9acd4c0301dce04caa22c04137aee8d5bd7cb1fc
Evan Lojewski <github@meklort.com>
2020-07-03 18:41:29 -0600

    config: Allow issuing a compact command from the host.

--- src/board/system76/common/smfi.c
+++ src/board/system76/common/smfi.c
@@ -298,6 +298,11 @@ void smfi_event(void) {
             case CMD_SET_CONFIG_VALUE:
                 smfi_cmd[1] = cmd_config_set_value_by_index();
                 break;
+            case CMD_COMPACT_CONFIG:
+                config_save_entries();
+                smfi_cmd[1] = RES_OK;
+                break;
+
 
 #endif // __SCRATCH__
             default:
